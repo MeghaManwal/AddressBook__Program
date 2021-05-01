aec0a06690d5035da32334763fccad8a32a3d01e
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-05-01 18:34:18 +0530

    Added:To create Contact in the AddressBook

--- AddressBookProgram.java
+++ AddressBookProgram.java
@@ -1,6 +1,20 @@
 
+import java.util.ArrayList;
+
 public class AddressBookProgram {
+
   public static void main(String[] args) {
-    System.out.println("Welcome to Address Book Program ");
+   ArrayList <String> AddressBook=new ArrayList <> ();
+
+    AddressBook.add("firstNmae: Neha ");
+    AddressBook.add("lastNmae: Sharma ");
+    AddressBook.add("Address: Electronic City ");
+    AddressBook.add("City: Bangalore ");
+    AddressBook.add("State: Karnataka ");
+    AddressBook.add("Zip: 560001 ");
+    AddressBook.add("PhoneNumber: 9876543210 ");
+    AddressBook.add("EmailId: neha@outlook.com ");
+
+    System.out.println("AddressBook: "+AddressBook);
  }
 }
